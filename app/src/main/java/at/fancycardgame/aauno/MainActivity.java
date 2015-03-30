0a3b8f4822e5693e7d792de2cc0158cef22901f5
TomR2015 <rauter.th@googlemail.com>
2015-03-30 19:45:14 +0200

    Project base to start from

--- app/src/main/java/at/fancycardgame/aauno/MainActivity.java
+++ app/src/main/java/at/fancycardgame/aauno/MainActivity.java
@@ -2,16 +2,13 @@ package at.fancycardgame.aauno;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
 }
