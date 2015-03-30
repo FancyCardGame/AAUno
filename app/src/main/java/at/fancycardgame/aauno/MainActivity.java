f55a5742db61acc342681154199c269d243cf44f
TomR2015 <rauter.th@googlemail.com>
2015-03-30 19:32:52 +0200

    Initial commit

---
+++ app/src/main/java/at/fancycardgame/aauno/MainActivity.java
@@ -0,0 +1,14 @@
+package at.fancycardgame.aauno;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
