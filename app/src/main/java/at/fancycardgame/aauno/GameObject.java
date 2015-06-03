8aa7d86f67157ec0f2e7eb812050b54ba6b4d19d
Dietmar <dietmar.jost@gmx.at>
2015-06-03 17:28:10 +0200

    Insert Gameboard_v1, Card Constants for data transfer

---
+++ app/src/main/java/at/fancycardgame/aauno/GameObject.java
@@ -0,0 +1,10 @@
+package at.fancycardgame.aauno;
+
+/**
+ * Created by Diete on 03.06.2015.
+ */
+public class GameObject {
+
+
+
+}
--- app/src/main/java/at/fancycardgame/aauno/toolbox/Constants.java
+++ app/src/main/java/at/fancycardgame/aauno/toolbox/Constants.java
@@ -15,5 +15,74 @@ public class Constants {
     public static final String PREP_TO_PLAY = "CNTDWN";
     public static final String WAIT_FOR_MIX = "WAIT_MIX";
 
+    // game cards constants
+
+    //red cards
+    public static final String red_0 = "r0";
+    public static final String red_1 = "r1";
+    public static final String red_2 = "r2";
+    public static final String red_3 = "r3";
+    public static final String red_4 = "r4";
+    public static final String red_5 = "r5";
+    public static final String red_6 = "r6";
+    public static final String red_7 = "r7";
+    public static final String red_8 = "r8";
+    public static final String red_9 = "r9";
+    public static final String red_skip = "rs";
+    public static final String red_plus2 = "rp";
+    public static final String red_turn = "rt";
+
+    //blue cards
+    public static final String blue_0 = "b0";
+    public static final String blue_1 = "b1";
+    public static final String blue_2 = "b2";
+    public static final String blue_3 = "b3";
+    public static final String blue_4 = "b4";
+    public static final String blue_5 = "b5";
+    public static final String blue_6 = "b6";
+    public static final String blue_7 = "b7";
+    public static final String blue_8 = "b8";
+    public static final String blue_9 = "b9";
+    public static final String blue_skip = "bs";
+    public static final String blue_plus2 = "bp";
+    public static final String blue_turn = "bt";
+
+    //yellow cards
+    public static final String yellow_0 = "y0";
+    public static final String yellow_1 = "y1";
+    public static final String yellow_2 = "y2";
+    public static final String yellow_3 = "y3";
+    public static final String yellow_4 = "y4";
+    public static final String yellow_5 = "y5";
+    public static final String yellow_6 = "y6";
+    public static final String yellow_7 = "y7";
+    public static final String yellow_8 = "y8";
+    public static final String yellow_9 = "y9";
+    public static final String yellow_skip = "ys";
+    public static final String yellow_plus2 = "yp";
+    public static final String yellow_turn = "yt";
+
+    //yellow cards
+    public static final String green_0 = "g0";
+    public static final String green_1 = "g1";
+    public static final String green_2 = "g2";
+    public static final String green_3 = "g3";
+    public static final String green_4 = "g4";
+    public static final String green_5 = "g5";
+    public static final String green_6 = "g6";
+    public static final String green_7 = "g7";
+    public static final String green_8 = "g8";
+    public static final String green_9 = "g9";
+    public static final String green_skip = "gs";
+    public static final String green_plus2 = "gp";
+    public static final String green_turn = "gt";
+
+    //special cards
+    public static final String color_change = "cc";
+    public static final String color_change_plus4 = "cp";
+
+
+
+
 
 }
