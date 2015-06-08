489f52e2bc03fa2c045f0a9231dc36722dc6533b
chrise2207 <christian.skina@outlook.com>
2015-06-08 15:14:29 +0200

    Usermanager update. Create user

---
+++ app/src/main/java/at/fancycardgame/aauno/ChangePwActivity.java
@@ -0,0 +1,64 @@
+package at.fancycardgame.aauno;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.shephertz.app42.paas.sdk.android.App42API;
+import com.shephertz.app42.paas.sdk.android.App42CallBack;
+import com.shephertz.app42.paas.sdk.android.user.UserService;
+
+/**
+ * Created by Christian on 01.06.2015.
+ */
+public class ChangePwActivity extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState){
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_changepwd_page);
+
+    }
+
+
+    public void onClick(View clickedView){
+        int clickedID = clickedView.getId();
+
+        if(clickedID == R.id.changePwdMP){
+            String oldPwd = ((TextView)findViewById(R.id.txtBoxOldPwd)).getText().toString();
+            String newPwd = ((TextView)findViewById(R.id.txtBoxNewPwd)).getText().toString();
+            changePassword(User.getUsername(), oldPwd, newPwd);
+        }
+
+
+    }
+
+    private void changePassword(String username, String oldPwd, String newPwd) {
+
+
+        UserService userService = App42API.buildUserService();
+
+        userService.changeUserPassword(username, oldPwd, newPwd, new App42CallBack() {
+
+
+
+
+
+           @Override
+            public void onSuccess(Object o) {
+
+                //show User a message that password has changed successfully
+            }
+
+            @Override
+            public void onException(Exception e) {
+
+                //show User a message that password has not changed
+            }
+        });
+    }
+
+}
